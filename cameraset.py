5d7c2db7da0635f0f844f6ea3fd6978752a6bf77
Kutay Macit <ucanbizon@gmail.com>
2019-09-25 18:13:24 -0600

    Update cameraset.py

--- cameraset.py
+++ cameraset.py
@@ -1,7 +1,10 @@
+
+
 import numpy as np
 import cv2
 import os
 import glob
+import bpy
 
 
 class Camera:
@@ -83,15 +86,16 @@ class Camera:
 
 
 # for intrinsic calibration
-cameras = [Camera(0, r'\A001_05290330_C001', False), Camera(1, r'\B001_07072140_C001', False),
-           Camera(2, r'\C001_06070320_C001', False), Camera(3, r'\D001_05061515_C001', False),
-           Camera(4, r'\E001_04210958_C001', False), Camera(5, r'\F001_05151133_C001', True),
-           Camera(6, r'\G001_05052127_C001', True), Camera(7, r'\H001_05191504_C001', False)]
+cameras = [Camera(0, r'\A001_05290330_C001', True), Camera(1, r'\B001_07072140_C001', True),
+           Camera(2, r'\C001_06070320_C001', True), Camera(3, r'\D001_05061515_C001', True),
+           Camera(4, r'\E001_04210958_C001', True), Camera(5, r'\F001_05151133_C001', True),
+           Camera(6, r'\G001_05052127_C001', True), Camera(7, r'\H001_05191504_C001', True)]
 
 cam2char = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
 directory = r'C:\Users\ucanb\Desktop\scan_mov\cameraset\Calib'
 extrinsic_filename = 'coord_sys_output.xml'
 se3_xml_path = os.path.join(directory, extrinsic_filename)
+deneme_matrix = None
 for camera in cameras:
     if camera.is_available:
         intrinsics_xml_path = directory + "\Intrinsics" + cam2char[camera.index] + '.xml'
@@ -100,9 +104,56 @@ for camera in cameras:
         d = cv_file.getNode('D1').mat()
         camera.set_M(M)
         camera.set_d(d)
-
+        
         cv_file = cv2.FileStorage(se3_xml_path, cv2.FILE_STORAGE_READ)
         se3 = cv_file.getNode('H_w_' + cam2char[camera.index]).mat()
+
         camera.set_SE3(se3)
+        print(se3)
+        
     else:
         print("* Camera[{}] set as not available")
+
+'''
+for camera in cameras:
+    name = cam2char[camera.index]
+    # create camera data
+    cam_data = bpy.data.cameras.new("{}.{:03d}".format(name, i))
+    # create object camera data and insert the camera data
+    cam_ob = bpy.data.objects.new("{}Cam.{:03d}".format(name, i), cam_data)
+    cam_ob.matrix_world
+    # link into scene
+    bpy.context.collection.objects.link(cam_ob)
+'''
+
+for camera in cameras:
+
+    name = cam2char[camera.index]
+
+    # create camera data
+    cam_data = bpy.data.cameras.new("{}.{:03d}".format(name, 0))
+
+    # create object camera data and insert the camera data
+    cam_ob = bpy.data.objects.new("{}Cam.{:03d}".format(name, 0), cam_data)
+
+    transformx180 = [[1,0,0,0],[0,-1,0,0],[0,0,-1,0],[0,0,0,1]]
+    cam_ob.matrix_world =  camera.SE3.transpose() 
+
+
+    if bpy.context.scene.unit_settings.length_unit == 'MILLIMETERS':
+        cam_ob.location[0] = cam_ob.location[0]/1000 
+        cam_ob.location[1] = cam_ob.location[1]/1000
+        cam_ob.location[2] = cam_ob.location[2]/1000
+    else:
+        sys.stderr.write("ERROR: CHANGE UNIT LENGTH TO MILLIMETERS")
+        sys.exit(1)    
+    cam_ob.rotation_euler[0] = cam_ob.rotation_euler[0] + np.pi
+    #cam_ob.rotation_euler[0] = cam_ob.rotation_euler[0]
+    #cam_ob.rotation_euler[1] = cam_ob.rotation_euler[1] 
+    #cam_ob.rotation_euler[2] = cam_ob.rotation_euler[2]
+    
+        
+    # link into scene
+    bpy.context.collection.objects.link(cam_ob)
+    
+
